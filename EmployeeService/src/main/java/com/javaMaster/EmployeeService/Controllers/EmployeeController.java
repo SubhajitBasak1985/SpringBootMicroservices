1eadb9fba2543add200a36990e452010c4ccef1f
Subhajit Basak <subhajit.lovesregina@gmail.com>
2026-03-23 01:39:36 -0600

    new update

--- EmployeeService/src/main/java/com/javaMaster/EmployeeService/Controllers/EmployeeController.java
+++ EmployeeService/src/main/java/com/javaMaster/EmployeeService/Controllers/EmployeeController.java
@@ -1,5 +1,43 @@
 package com.javaMaster.EmployeeService.Controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.javaMaster.EmployeeService.Entities.Employee;
+
+
+@RestController
+@RequestMapping("/Employee")
 public class EmployeeController {
+	
+	@PostMapping("/Add")
+	public Employee addEmployee( @RequestBody Employee employee)
+	{
+		return null;
+	}
+	
+	@GetMapping("/GetEmployee/{empId}")
+	public Employee getEmployeeById(@PathVariable Long employeeId)
+	{
+		return null;
+	}
+	
+	@PutMapping("/UpdateEmployee/{empId}")
+	public Employee updateEmployee(@PathVariable Long employeeId, Employee employee)
+	{
+		return null;
+	}
+	
+	@DeleteMapping("/DeleteEmployee/{empId}")
+	public void addTwoNumber(@PathVariable Long employeeId)
+	{
+		
+	}
+	
 
 }
